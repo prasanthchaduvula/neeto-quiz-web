043ecc044f63aae535e71fb2768b592ceb0c693f
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-13 21:22:37 +0530

    Ran create users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_133002) do
+ActiveRecord::Schema.define(version: 2020_10_13_153427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number", default: "", null: false
+  end
 
 end
