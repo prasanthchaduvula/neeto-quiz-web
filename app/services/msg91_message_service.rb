6041d4c75135534fea223c53629b204a31a921ac
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-14 21:17:49 +0530

    Added typhoeus gem and made otp requests using it

--- app/services/msg91_message_service.rb
+++ app/services/msg91_message_service.rb
@@ -4,22 +4,19 @@ class Msg91MessageService
   def send_otp(phone_number)
     url = URI("https://api.msg91.com/api/v5/otp?authkey=344320ADzUdaWyKVf5f8684dbP1&template_id=5f868692317bbf40b878216c&mobile=#{phone_number}")
 
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Get.new(url)
-    response = http.request(request)
-    JSON.parse(response.body)
+    send_response(url)
   end
 
   def verify_otp(phone_number, otp)
     url = URI("https://api.msg91.com/api/v5/otp/verify?mobile=#{phone_number}&otp=#{otp}&authkey=344320ADzUdaWyKVf5f8684dbP1")
 
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Post.new(url)
-    reqres = http.request(request)
-    JSON.parse(reqres.body)
+    send_response(url)
+  end
+
+  def send_response(url)
+    response = Typhoeus::Request.new(
+      url,
+    ).run
+    JSON.parse(response.body)
   end
 end
