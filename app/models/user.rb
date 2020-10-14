1b059a6c9d885e03b16c1c8088370c7d514e36b3
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-14 16:37:09 +0530

    Added devise to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :trackable, :validatable, :rememberable
+  devise :database_authenticatable, authentication_keys: [:phone_number]
+
   validates :first_name, :last_name, presence: true, on: :update
   validates :phone_number, presence: true, numericality: true, length: { is: 13 }, uniqueness: true
+
+  def email_required?
+    false
+  end
+
+  def password_required?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
+
+  def will_save_change_to_id?
+    false
+  end
 end
