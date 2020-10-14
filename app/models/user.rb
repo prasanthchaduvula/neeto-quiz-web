9d9d289a435e75345d7dec3672ed5d7f4ff5c92e
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-14 07:24:00 +0530

    Added validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :first_name, :last_name, :phone_number, presence: true
+  validates :phone_number, numericality: true, length: { is: 10 }, uniqueness: true
 end
