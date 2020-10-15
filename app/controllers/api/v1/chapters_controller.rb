1ccee71a71f7f1a016544b3f6f4ead1c37f82160
ayushman-parasar <learnertsk@gmail.com>
2020-10-15 13:19:06 +0530

    added CURD actions to chapters controller

--- app/controllers/api/v1/chapters_controller.rb
+++ app/controllers/api/v1/chapters_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Api::V1::ChaptersController < Api::V1::BaseController
-  before_action :find_course, only: [:show, :destroy, :update, :create]
+  before_action :find_course, only: :create
+  before_action :find_chapter, only: [:show, :destroy, :update]
 
   def create
     chapter = @course.chapters.new(chapter_params)
@@ -12,8 +13,36 @@ class Api::V1::ChaptersController < Api::V1::BaseController
     end
   end
 
+  def update
+    if @chapter.update(chapter_params)
+      render status: :ok, json: { notice: "Chapter updated successfully", chapter: @chapter }
+    else
+      render status: :unprocessable_entity, json: { errors: @chapter.errors.full_messages }
+    end
+  end
+
+  def show
+    if @chapter
+      render status: :ok, json: @chapter
+    else
+      render status: :not_found, json: { errors: ["Chapter with id #{params[:id]} not found"] }
+    end
+  end
+
+  def destroy
+    if @chapter.destroy
+      render status: :ok, json: @chapter
+    else
+      render status: :unprocessable_entity, json: { errors: @chapter.errors.full_messages }
+    end
+  end
+
   private
     def chapter_params
       params.require(:chapter).permit(:name)
+    end
+
+    def find_chapter
+      @chapter = Chapter.find_by_id(params[:id])
     end
 end
