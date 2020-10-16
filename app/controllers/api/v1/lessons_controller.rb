b9236f12a82b1a308a015547b2140a11bbd2ff07
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-16 11:44:44 +0530

    Update the lesson

--- app/controllers/api/v1/lessons_controller.rb
+++ app/controllers/api/v1/lessons_controller.rb
@@ -2,20 +2,33 @@
 
 class Api::V1::LessonsController < Api::V1::BaseController
   before_action :load_chapter
+  before_action :load_lesson, except: [:create]
 
   def create
-    lesson = @chapter.lessons.new(lesson_params)
-    if lesson.save
-      render status: :ok, json: { notice: "Lesson created successfully", lesson: lesson }
+    @lesson = @chapter.lessons.new(lesson_params)
+    if @lesson.save
+      render json: { lesson: @lesson }, status: :ok
     else
-      render status: :unprocessable_entity, json: { errors: lesson.errors.full_messages }
+      render status: :unprocessable_entity, json: { errors: @lesson.errors.full_messages }
+    end
+  end
+
+  def update
+    if @lesson.update(lesson_params)
+      render json: { lesson: @lesson }, status: :ok
+    else
+      render json: { errors: @lesson.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
 
     def lesson_params
-      params.request(:lesson).permit(:name, :description, :content, :content_type)
+      params.require(:lesson).permit(:name, :description, :content, :content_type)
+    end
+
+    def load_lesson
+      @lesson = @chapter.lessons.find_by!(id: params[:id])
     end
 
     def load_chapter
