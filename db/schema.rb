57f010c89176eb522d2f80bd5c5ac5fc2fd13d23
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-16 13:04:36 +0530

    Added lesson is_published to lessons table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_040612) do
+ActiveRecord::Schema.define(version: 2020_10_16_073158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_10_16_040612) do
     t.datetime "updated_at", precision: 6, null: false
     t.uuid "chapter_id"
     t.integer "lesson_type"
+    t.boolean "is_published", default: false, null: false
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
