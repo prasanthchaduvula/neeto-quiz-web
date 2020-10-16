dd5de9f25c3d1b5d20bdf46b932509a0760b7397
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-16 22:23:59 +0530

    Updated OR condition in file validation

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -10,5 +10,5 @@ class Lesson < ApplicationRecord
   validates :name, :lesson_type, presence: true
   validates :content, presence: true, if: -> { lesson_type == "youtube" }
   validates :file, content_type: { in: %w[image/jpeg image/jpg image/gif image/png application/pdf], message: "must be a valid image or pdf format" },
-      presence: true, if: -> { lesson_type == "image" || "pdf" }
+      presence: true, if: -> { lesson_type == "image" || lesson_type == "pdf" }
 end
