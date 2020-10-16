a44225da2133b89977fa5800b10f862fc9f0ee48
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-16 11:13:05 +0530

    Create a lesson

---
+++ app/controllers/api/v1/lessons_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Api::V1::LessonsController < Api::V1::BaseController
+  def create
+    lesson = @chapter.lessons.new(lesson_params)
+    if lesson.save
+      render status: :ok, json: { notice: "Lesson created successfully", lesson: lesson }
+    else
+      render status: :unprocessable_entity, json: { errors: lesson.errors.full_messages }
+    end
+  end
+
+  private
+
+    def lesson_params
+      params.request(:lesson).permit(:name, :description, :content, :content_type)
+    end
+
+    def load_chapter
+      @chapter = Chapter.find_by!(id: params[:chapter_id])
+    end
+end
