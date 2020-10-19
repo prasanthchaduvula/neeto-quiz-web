773092758af4f553770a6aa42cabcdeb3b707ce3
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-19 15:55:42 +0530

    Made course and user associations using course_student model

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,10 @@
 
 class Course < ApplicationRecord
   has_many :chapters, dependent: :destroy
+  has_many :course_students, dependent: :destroy
+  has_many :joined_students, through: :course_students,  source: :user
+
   belongs_to :user
+
   validates :name, :description, :user, presence: true
 end
