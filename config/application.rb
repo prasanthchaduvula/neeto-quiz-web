7242f82ae32f15206d712adc6c9fa5a979aa38f5
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-21 17:04:29 +0530

    Added i18 configuration in application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module NitroAcademy
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
     end
+    
+    config.i18n.available_locales = [:en]
+    config.i18n.default_locale = :en
   end
 end
