af500789140fc48110f8aa378758f1a2f5a4bbc9
ayushman-parasar <learnertsk@gmail.com>
2020-10-21 18:18:13 +0530

    edit button clicked

--- app/javascript/src/components/Dashboard/Courses/ListCourses.jsx
+++ app/javascript/src/components/Dashboard/Courses/ListCourses.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { Button } from "nitroui";
+
 export default function ListCourses(props) {
   return (
     <div className="bg-gray">
-      LIST
       <ul>
         {props.courses.map(course => {
           return (
             <li key={course.id}>
-              <div className="p-5 bg-white rounded shadow"></div>
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="px-6 py-4">
+                  <div className="text-xl mb-2">{course.name}</div>
+                  <p className="text-gray-700 text-base">
+                    {course.description}
+                  </p>
+                </div>
+                <div className="px-6 pt-4 pb-2">
+                  <span className="inline-block  px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <Button type="button" label="Edit" />
+                  </span>
+                </div>
+              </div>
             </li>
           );
         })}
