4266229637461aff834c9db859b3d9e32691ad89
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-21 17:46:15 +0530

    AddUniqueNameToChapter migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_101146) do
+ActiveRecord::Schema.define(version: 2020_10_21_121232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_10_19_101146) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["course_id"], name: "index_chapters_on_course_id"
+    t.index ["name", "course_id"], name: "index_chapters_on_name_and_course_id", unique: true
   end
 
   create_table "course_students", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
