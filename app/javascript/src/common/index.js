1ee81e4b99ea0f7ad708b7a24b61fc15daac116a
ayushman-parasar <learnertsk@gmail.com>
2020-10-22 03:17:25 +0530

    removed unwanted lines of code

--- app/javascript/src/common/index.js
+++ app/javascript/src/common/index.js
@@ -1,26 +1,4 @@
 import { Toastr as ToastrComponent } from "nitroui";
-// import { toast, Slide } from "react-toastify";
-
-// const showToastr = message => {
-//   toast.success(<ToastrComponent type="success" message={message} />, {
-//     position: toast.POSITION.BOTTOM_CENTER,
-//     transition: Slide,
-//   });
-// };
-
-// const isError = e => e && e.stack && e.message;
-
-// export const showErrorToastr = error => {
-//   let errorMessage = error;
-//   if (isError(error)) {
-//     errorMessage = error.message;
-//   }
-//   toast.error(<ToastrComponent type="error" message={errorMessage} />, {
-//     position: toast.POSITION.BOTTOM_CENTER,
-//     transition: Slide,
-//   });
-// };
-
 export const showToastr = (type, ...rest) => {
   if (type === "error") {
     ToastrComponent[type](...rest);
@@ -32,8 +10,3 @@ export const showToastr = (type, ...rest) => {
     );
   }
 };
-
-// export const Toastr = {
-//   success: showToastr,
-//   error: showErrorToastr,
-// };
