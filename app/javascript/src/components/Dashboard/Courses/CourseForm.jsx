0596ffb518ffeb2f47ecd2fc32dff5988cbd0b35
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-25 16:40:11 +0530

    Updated course form with formik and added validations

--- app/javascript/src/components/Dashboard/Courses/CourseForm.jsx
+++ app/javascript/src/components/Dashboard/Courses/CourseForm.jsx
@@ -1,91 +1,126 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Button } from "nitroui";
-import CourseApi from "../../../apis/courses";
+// import CourseApi from "../../../apis/courses";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+
 export default function CourseForm(props) {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
+  const initialValues = {
+    name: "",
+    description: "",
+    price: "",
+  };
 
-  props.isCreateForm
-    ? null
-    : useEffect(() => {
-        setName(props.courseName);
-        setDescription(props.courseDescription);
-      }, [props.courseName, props.courseDescription]);
+  const validationSchema = yup.object().shape({
+    name: yup.string().required("Required *"),
+    description: yup.string().required("Required *"),
+  });
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const payload = {
-      course: {
-        name,
-        description,
-      },
-    };
-    const sendRequest = payload => {
-      return props.courseId
-        ? CourseApi.updateCourse(props.courseId, payload)
-        : CourseApi.createCourse(payload);
-    };
-    sendRequest(payload).then(response => {
-      if (props.isCreateForm) {
-        props.refetch();
-        props.onClose();
-      } else {
-        props.setCourse(response.data.course_details);
-        props.onClose();
-      }
-    });
+  // props.isCreateForm
+  //   ? null
+  //   : useEffect(() => {
+  //       setName(props.courseName);
+  //       setDescription(props.courseDescription);
+  //     }, [props.courseName, props.courseDescription]);
+
+  const handleSubmit = () => {
+    // const payload = {
+    //   course: {
+    //     name,
+    //     description,
+    //   },
+    // };
+    // const sendRequest = payload => {
+    //   return props.courseId
+    //     ? CourseApi.updateCourse(props.courseId, payload)
+    //     : CourseApi.createCourse(payload);
+    // };
+    // sendRequest(payload).then(response => {
+    //   if (props.isCreateForm) {
+    //     props.refetch();
+    //     props.onClose();
+    //   } else {
+    //     props.setCourse(response.data.course_details);
+    //     props.onClose();
+    //   }
+    // });
   };
   return (
-    <form
+    <Formik
+      initialValues={initialValues}
       onSubmit={handleSubmit}
+      validationSchema={validationSchema}
       className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
     >
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlor="name"
-        >
-          Name of the course
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username"
-          type="text"
-          placeholder="Name"
-          onChange={e => setName(e.target.value)}
-          value={name}
-        />
-      </div>
-      <div className="mb-6">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="description"
-        >
-          Description
-        </label>
-        <textarea
-          className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          placeholder="Description"
-          onChange={e => setDescription(e.target.value)}
-          value={description}
-        />
-      </div>
-      <div className="absolute bottom-0 left-0 w-full bg-white nui-pane--footer">
-        <Button
-          onClick={props.onClose}
-          label="Cancel"
-          size="large"
-          style="secondary"
-        />
+      <Form>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlor="name"
+          >
+            Name of the course
+          </label>
+          <ErrorMessage name="name" component="div" className="text-red-600" />
+          <Field
+            type="text"
+            id="name"
+            name="name"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
+          />
+        </div>
 
-        <Button
-          type="submit"
-          label="Submit"
-          size="large"
-          style="primary"
-          className="ml-2"
-        />
-      </div>
-    </form>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlor="description"
+          >
+            Description
+          </label>
+          <ErrorMessage
+            name="description"
+            component="div"
+            className="text-red-600"
+          />
+          <Field
+            as="textarea"
+            id="description"
+            name="description"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlor="price"
+          >
+            Price
+          </label>
+          <Field
+            type="number"
+            id="price"
+            name="price"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
+          />
+        </div>
+
+        <div className="absolute bottom-0 left-0 w-full bg-white nui-pane--footer">
+          <Button
+            onClick={props.onClose}
+            label="Cancel"
+            size="large"
+            style="secondary"
+          />
+
+          <Button
+            type="submit"
+            label="Submit"
+            size="large"
+            style="primary"
+            className="ml-2"
+          />
+        </div>
+      </Form>
+    </Formik>
   );
 }
