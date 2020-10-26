3c91e9b54ecccaf111636a78990a4057d8383876
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-26 20:43:52 +0530

    Added return statement to axios

--- app/javascript/src/apis/courses.js
+++ app/javascript/src/apis/courses.js
@@ -1,18 +1,21 @@
 import axios from "axios";
 
-export const getCourses = () => axios.get("/api/v1/courses");
+export const getCourses = () => {
+  return axios.get("/api/v1/courses");
+};
 
 export const getCourse = id => {
   return axios.get(`/api/v1/courses/${id}`);
 };
 
-export const createCourse = payload => axios.post("/api/v1/courses", payload);
+export const createCourse = payload => {
+  return axios.post("/api/v1/courses", payload);
+};
 
-export const updateCourse = (id, payload) =>
-  axios.patch(`/api/v1/courses/${id}`, payload);
+export const updateCourse = (id, payload) => {
+  return axios.patch(`/api/v1/courses/${id}`, payload);
+};
 
-export const deleteCourse = id => axios.delete(`/api/v1/courses/${id}`);
-
-export default {
-  deleteCourse,
+export const deleteCourse = id => {
+  return axios.delete(`/api/v1/courses/${id}`);
 };
