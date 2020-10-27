90b98e8459743189096c576dcbef52f2206aa25b
ayushman-parasar <learnertsk@gmail.com>
2020-10-27 18:43:18 +0530

    after rebase

--- app/javascript/src/apis/chapters.js
+++ app/javascript/src/apis/chapters.js
@@ -11,19 +11,8 @@ export const updateChapter = (courseId, chapterId, payload) => {
   );
 };
 
-<<<<<<< HEAD
 export const deleteChapter = (courseId, chapterId) => {
   return axios.delete(`/api/v1/courses/${courseId}/chapters/${chapterId}`);
-=======
-const deleteChapter = (courseId, chapterId) =>
-  axios.delete(`/api/v1/courses/${courseId}/chapters/${chapterId}`);
-
-export default {
-  fetchChapter,
-  createChapter,
-  updateChapter,
-  deleteChapter,
->>>>>>> chapter controller updated to include lessons in show and update action, made  chapter api routes, updated chapter form for edit chapter functionality, created editchapter sectionor pane, created show chapter component and completed functionality of chapter edit and delete, made minor ui changes to indivitualcourse component
 };
 
 export const fetchChapter = (courseId, chapterId) =>
--- app/views/api/v1/courses/show.json.jbuilder
+++ app/views/api/v1/courses/show.json.jbuilder
@@ -1,16 +1,11 @@
 # frozen_string_literal: true
 
 json.course @course
-<<<<<<< HEAD
 json.chapters @course.chapters.map do |chapter|
   json.chapter chapter
   json.lessons chapter.lessons.includes([:file_attachment]).map { |l| l.attributes.merge(file: l.file.present? && Rails.application.routes.url_helpers.polymorphic_path(l.file)) }
-=======
-json.chapters @course.chapters.includes(:lessons).map do |chapter|
-  json.chapter chapter
-  json.lessons chapter.lessons
->>>>>>> Removed course view service and updated with jbuilder
-endjson.students @course.joined_students.map do |student|
+end
+json.students @course.joined_students.map do |student|
   json.name student.name
   json.phone_number student.phone_number
   json.id student.id
