eb6e541c27c4bc87a1fd9a75f2f596104e5c9028
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-10-28 15:22:39 +0530

    Used Input with prefix instead of Field

--- app/javascript/src/components/Dashboard/Students/Form.jsx
+++ app/javascript/src/components/Dashboard/Students/Form.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "nitroui";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Input } from "nitroui/formik";
+import { Formik, Form, Field } from "formik";
 import * as yup from "yup";
 import { showToastr } from "common";
 import { addStudent } from "apis/courses";
@@ -39,29 +40,20 @@ export default function StudentForm({ onClose, course, fetchSingleCourse }) {
         return (
           <Form>
             <div className="mb-4">
-              <div className="form-control">
-                <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="phone_number"
-                >
-                  Enter the Student Phone Number you want to add
-                </label>
-                <ErrorMessage
-                  name="phone_number"
-                  component="div"
-                  className="text-red-600"
-                />
-                <div className="md:flex items-center mt-2 ">
-                  <span className="text-gray-500 mr-2">+91</span>
-                  <Field
-                    type="number"
-                    id="phone_number"
-                    name="phone_number"
-                    className="border"
-                    placeholder="Enter phone number"
-                  />
-                </div>
-              </div>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="phone_number"
+              >
+                Enter the Student Phone Number you want to add
+              </label>
+              <Input
+                type="number"
+                id="phone_number"
+                name="phone_number"
+                prefix="+91"
+                className="border"
+                placeholder="Enter phone number"
+              />
             </div>
 
             <div className="mt-4">
