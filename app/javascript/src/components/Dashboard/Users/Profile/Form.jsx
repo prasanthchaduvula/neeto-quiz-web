735fe94fd6da6c0f28eeb2fb1ac40d8214c4ffad
ayushman-parasar <learnertsk@gmail.com>
2020-11-02 11:30:54 +0530

    user_id is stored as string in previous pr hence removed the json.parse function

--- app/javascript/src/components/Dashboard/Users/Profile/Form.jsx
+++ app/javascript/src/components/Dashboard/Users/Profile/Form.jsx
@@ -15,7 +15,7 @@ export default function GeneralSettings() {
   }, []);
 
   const loadProfile = () => {
-    const user_id = JSON.parse(localStorage.getItem("user_id"));
+    const user_id = localStorage.getItem("user_id");
 
     getUser(user_id).then(response => {
       setInitialValues(response.data.user);
