dbfcda1fc8c122d1d28469be50f235c7f0513d4e
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2020-11-02 14:31:28 +0530

    Added created courses tab to coursesA

--- app/javascript/src/components/Dashboard/Courses/index.jsx
+++ app/javascript/src/components/Dashboard/Courses/index.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { PageHeading } from "nitroui/layouts";
-import { Button, PageLoader } from "nitroui";
+import { Button, PageLoader, Tab } from "nitroui";
 import { getCourses } from "apis/courses";
 import ListCourses from "./ListCourses";
 import CoursePane from "./Pane";
+import { TABS } from "./constants";
 
 export default function Courses() {
   const [coursePane, setCoursePane] = useState(false);
   const [courses, setCourses] = useState({});
+  const [activeTab, setActiveTab] = useState("createdCourses");
 
   useEffect(() => {
     fetchCourses();
@@ -30,13 +32,35 @@ export default function Courses() {
         )}
       />
       {courses ? (
-        <ListCourses
-          courses={courses.courses_created}
-          joinedCourses={courses.courses_joined}
-        />
+        <>
+          <Tab className="px-6 -mx-4 border-b border-gray-200">
+            <Tab.Item
+              icon="ri-user-line"
+              onClick={() => {
+                if (activeTab !== TABS.createdCourses) {
+                  setActiveTab(TABS.createdCourses);
+                }
+              }}
+              active={activeTab === TABS.createdCourses}
+            >
+              Created Courses
+            </Tab.Item>
+          </Tab>
+          <div className="flex flex-row items-start justify-center flex-grow">
+            <div className="w-full h-full md:w-140">
+              {activeTab === TABS.createdCourses && (
+                <ListCourses
+                  courses={courses.courses_created}
+                  joinedCourses={courses.courses_joined}
+                />
+              )}
+            </div>
+          </div>
+        </>
       ) : (
         <PageLoader />
       )}
+
       <CoursePane
         showPane={coursePane}
         setShowPane={setCoursePane}
